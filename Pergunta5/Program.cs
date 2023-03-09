63b689350e050e45cb2a641b10aae59787430aee
Wagner Santos <wagnerstss@gmail.com>
2023-03-09 16:18:33 -0300

    String Invert

---
+++ Pergunta5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+
+namespace Pergunta5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite uma palavra: ");
+            string p = Console.ReadLine();
+            StringBuilder sb = new StringBuilder();
+
+            for (var i = p.Length - 1; i >= 0; i--)
+            {
+                sb.Append(p[i]);
+
+            }
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
